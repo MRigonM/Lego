7275bc2c8926adac01adf336946264d2b3ced0d1
Rigonm2 <rigonmej@gmail.com>
2024-08-29 01:05:17 +0300

    feature/Lego-6-Added removed items

---
+++ API/Program.cs
@@ -0,0 +1,60 @@
+using API.Errors;
+using API.Extensions;
+using API.Middleware;
+using Core.Entities;
+using Core.Entities.Identity;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityService(builder.Configuration);
+builder.Services.AddSwaggerDocumentation();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+app.UseStatusCodePagesWithReExecute("/errors/{0}");
+
+app.UseSwaggerDocumentation();
+
+app.UseStaticFiles();
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthentication();// authtic gjith shkon para authriz
+app.UseAuthorization();// se tani merr 401 error 
+
+app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var context = services.GetRequiredService<StoreContext>();
+var identityContext = services.GetRequiredService<AppIdentityDbContext>();
+var userManager = services.GetRequiredService<UserManager<AppUser>>();
+var logger = services.GetRequiredService<ILogger<Program>>();
+
+try
+{
+    await context.Database.MigrateAsync();
+    await identityContext.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
+    await AppIdentityDbContextSeed.SeedUsersAsync(userManager);
+}
+catch (Exception x)
+{
+    
+    logger.LogError(x, "An error occured during migration");
+}
+
+app.Run();
