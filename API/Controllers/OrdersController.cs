839d5f3aba44417d15de46acfb6273d7088062d6
Sara <su69128@ubt-uni.net>
2024-09-14 16:29:13 +0200

    Lego-23- Added StripeWebhook method at PaymentController and updated IPaymentService

--- API/Controllers/OrdersController.cs
+++ API/Controllers/OrdersController.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
 using API.Dtos;
 using API.Errors;
 using API.Extensions;
--- API/Controllers/PaymentsController.cs
+++ API/Controllers/PaymentsController.cs
@@ -3,15 +3,20 @@ using Core.Entities;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Stripe;
+using Order = Core.Entities.OrderAggregate.Order;
 
 namespace API.Controllers;
 
 public class PaymentsController: BaseApiController
 {
+    private const string WhSecret = "whsec_2ed1310b90f8b70e27ce5f60186d7262b5190bcd9ffec8fcb4478078a30e28e3";
     private readonly IPaymentService _paymentService;
+    private readonly ILogger<PaymentsController> _logger;
 
-    public PaymentsController(IPaymentService paymentService)
+    public PaymentsController(IPaymentService paymentService, ILogger<PaymentsController> logger)
     {
+        _logger = logger;
         _paymentService = paymentService;
     }
     
@@ -24,5 +29,56 @@ public class PaymentsController: BaseApiController
         if (basket == null) return BadRequest(new ApiResponse(400, "Problem with your basket"));
 
         return basket;
+    }
+    
+    [HttpPost("webhook")]
+    public async Task<ActionResult> StripeWebhook()
+    {
+        var json = await new StreamReader(Request.Body).ReadToEndAsync();
+    
+        try
+        {
+            var stripeEvent = EventUtility.ConstructEvent(
+                json,
+                Request.Headers["Stripe-Signature"],
+                WhSecret
+            );
+
+            PaymentIntent intent;
+            Order order;
+
+            switch (stripeEvent.Type)
+            {
+                case "payment_intent.succeeded":
+                    intent = (PaymentIntent)stripeEvent.Data.Object;
+                    _logger.LogInformation("Payment Succeeded: {Id}", intent.Id);
+                    order = await _paymentService.UpdateOrderPaymentSucceeded(intent.Id);
+                    _logger.LogInformation("Order updated to payment received: {Id}", order.Id);
+                    break;
+
+                case "payment_intent.payment_failed":
+                    intent = (PaymentIntent)stripeEvent.Data.Object;
+                    _logger.LogInformation("Payment Failed: {Id}", intent.Id);
+                    order = await _paymentService.UpdateOrderPaymentFailed(intent.Id);
+                    _logger.LogInformation("Order updated to payment failed: {Id}", order.Id);
+                    break;
+
+                default:
+                    _logger.LogWarning("Unhandled event type: {Type}", stripeEvent.Type);
+                    break;
+            }
+
+            return Ok();
+        }
+        catch (StripeException ex)
+        {
+            _logger.LogError("Stripe exception: {Message}", ex.Message);
+            return BadRequest();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("Webhook processing error: {Message}", ex.Message);
+            return StatusCode(500, "Webhook processing failed");
+        }
     }
 }
